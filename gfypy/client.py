b33527dc9d4db767bddd813e49bfd7d89332b0c0
Noah <noah@dreckbu.de>
2020-05-15 20:48:40 +0200

    Add local webserver that waits for the gfycat auth callback

--- gfypy/client.py
+++ gfypy/client.py
@@ -1,6 +1,11 @@
-import requests
-import webbrowser
 import json
+import webbrowser
+import time
+from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
+from threading import Thread
+from urllib.parse import urlparse
+
+import requests
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -10,6 +15,19 @@ class BearerAuth(requests.auth.AuthBase):
     def __call__(self, r):
         r.headers["authorization"] = "Bearer " + self.token
         return r
+
+
+class AuthCallbackRequestHandler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<h2>You may close this window now!</h2>", 'UTF-8'))
+        query = urlparse(self.path).query
+        query_components = dict(qc.split('=') for qc in query.split('&'))
+        code = query_components['code']
+        self.server.code = code
 
 
 class Gfypy:
@@ -25,10 +43,20 @@ class Gfypy:
         self.access_token = None
         self.auth = None
 
-    def _get_oauth_token(self):
+    def initial_auth(self):
+        self._get_oauth_token(self._get_oauth_code())
+        print(self.auth)
+
+    def _get_oauth_code(self):
         """
         Gets authorization token
         """
+        server = ThreadingHTTPServer(('localhost', 8000), AuthCallbackRequestHandler)
+        server.code = None
+        server_thread = Thread(target=server.serve_forever)
+        server_thread.daemon = False
+        server_thread.start()
+
         auth_url = f'{Gfypy.AUTH_ENDPOINT}?client_id={self.client_id}' \
                    f'&scope=all' \
                    f'&state=Gfypy' \
@@ -36,9 +64,16 @@ class Gfypy:
                    f'&redirect_uri={Gfypy.REDIRECT_URI}'
         webbrowser.open(auth_url)
 
-        print('Please paste the Code that appears after clicking "OK": ')
-        code = input()
+        while server.code is None:
+            time.sleep(1)
 
+        assassin = Thread(target=server.shutdown)
+        assassin.daemon = True
+        assassin.start()
+
+        return server.code
+
+    def _get_oauth_token(self, code):
         payload = {
             'code': code,
             'client_id': self.client_id,
--- tests.py
+++ tests.py
@@ -3,6 +3,6 @@ from conf import CLIENT_ID, CLIENT_SECRET
 
 if __name__ == '__main__':
     gfypy = Gfypy(CLIENT_ID, CLIENT_SECRET)
-    gfypy._get_oauth_token()
+    gfypy.initial_auth()
     # gfypy.get_key('this is a test', ['tag', 'tag2', 'tag with 한글'])
-    gfypy.upload_from_file('this is a test', ['tag', 'tag2', 'tag with 한글'], 'unamused_fin_longest.gif')
+    # gfypy.upload_from_file('this is a test', ['tag', 'tag2', 'tag with 한글'], 'unamused_fin_longest.gif')
