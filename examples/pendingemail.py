fe669939d2edcf8a9d16c511c5f8439db70ea831
badmagick329 <63713349+badmagick329@users.noreply.github.com>
2020-05-19 02:11:15 +0100

    Create pendingemail.py

---
+++ examples/pendingemail.py
@@ -0,0 +1,23 @@
+from gfypy.client import Gfypy
+from conf import CLIENT_ID, CLIENT_SECRET
+import time,json,os
+
+if __name__ == '__main__':
+    gfypy = Gfypy(CLIENT_ID, CLIENT_SECRET, './creds.json')
+    gfypy.authenticate()
+    #gfypy.initial_auth()
+    #gfypy._auth_to_disk()
+    
+    #go through own feed, write a text file with all gfys stuck on pending
+    def pendingcheck():
+        start = time.time()
+        gfycats = gfypy.get_own_feed(limit=10000, gatekeeper=5)
+        end = time.time()
+        print(f'{end - start}s elapsed')
+        tosend="Hi,\n\nThese gfys are stuck on pending review\n\n"
+        for i in range(len(gfycats)):
+            tosend+=f"{i+1} https://www.gfycat.com/{gfycats[i]['gfyId']}\n"
+        tosend+="\nThanks"
+        with open ('gfycatemail.txt', 'w') as f:
+            f.write(tosend)
+    pendingcheck()
